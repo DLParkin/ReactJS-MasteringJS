32e81f6008b58235db6f6650a7fd71167c46de99
David <davidsatwork2@hotmail.com>
2020-03-02 16:18:13 +0800

    list characters in characterList from state

--- supersquad/src/components/CharacterList.js
+++ supersquad/src/components/CharacterList.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CharacterList extends Component {
   render() {
+    const characters = this.props.characters;
+
     return (
       <div>
         Characters
+        <ul>
+          {characters.map(character => {
+            return (
+              <li key={character.id}>
+                <div>{character.name}</div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default connect()(CharacterList);
+function mapStateToProps(state) {
+  return {
+    characters: state.characters
+  };
+}
+
+export default connect(mapStateToProps, null)(CharacterList);
