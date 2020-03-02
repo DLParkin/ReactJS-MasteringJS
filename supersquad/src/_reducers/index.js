ae0ae91d97626a982a4a81a12bb0ad6e7dd5361e
David <davidsatwork2@hotmail.com>
2020-03-02 13:52:18 +0800

    create first store, add devtools since it is missing

---
+++ supersquad/src/_reducers/index.js
@@ -0,0 +1,10 @@
+import charactersJSON from "../_data/characters.json";
+
+function characters(state = charactersJSON, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default characters;
--- supersquad/src/index.js
+++ supersquad/src/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import rootReducer from "./_reducers";
+
+const store = createStore(
+  rootReducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
