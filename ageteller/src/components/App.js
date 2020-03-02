2f055e9ba4efb690985aa6c4a82e5a25cb3bb930
David <davidsatwork2@hotmail.com>
2020-03-02 13:00:17 +0800

    finish ageteller, nothing out of the ordinary.. the keyframe was kinda neat

--- ageteller/src/components/App.js
+++ ageteller/src/components/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
-import './App.css';
+import "./App.css";
+
+import AgeStats from "./AgeStats";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: "",
+      birthday: "1992-06-21",
+      showStats: false
+    };
+  }
+
+  changeBirthday() {
+    this.setState({ birthday: this.state.newDate, showStats: true });
+  }
+
   render() {
     return (
       <div className="App">
-        <Form inline>
-          <h2>Input your birthday!</h2>
-          <FormControl type="date" /><Button>Submit</Button>
+        <h2 className="birthday-font">Input your birthday!</h2>
+        <Form inline className="birthday-form">
+          <FormControl
+            type="date"
+            onChange={e => this.setState({ newDate: e.target.value })}
+          />
+          <Button
+            className="birthday-button"
+            onClick={() => this.changeBirthday()}
+          >
+            Submit
+          </Button>
         </Form>
+        {this.state.showStats && (
+          <div className="fade age-stats">
+            <AgeStats date={this.state.birthday} />
+          </div>
+        )}
       </div>
     );
   }
