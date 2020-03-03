2fa47ab890338f047e25c1f71559e05e8f548b5c
David <davidsatwork2@hotmail.com>
2020-03-03 21:28:40 +0800

    create basic fetch for recipes

--- recipefinder/src/components/SearchRecipes.js
+++ recipefinder/src/components/SearchRecipes.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
-import { Form, FormGroup, FormControl, Button, FormLabel } from "react-bootstrap";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Button,
+  FormLabel
+} from "react-bootstrap";
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ingredients: "",
+      dish: ""
+    };
+  }
+
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+    console.log(url)
+    fetch(url, {
+      method: 'GET'
+    }).then(response => response.json())
+      .then(json => console.log("recipes", json));
+  }
+
   render() {
     return (
       <Form inline>
         <FormGroup>
           <FormLabel>Ingredients </FormLabel>{" "}
-          <FormControl type="text" placeholder="garlic, chicken" />
+          <FormControl
+            type="text"
+            placeholder="garlic, chicken"
+            onChange={e => this.setState({ ingredients: e.target.value })}
+          />
         </FormGroup>{" "}
         <FormGroup>
           <FormLabel>Dish </FormLabel>{" "}
-          <FormControl type="text" placeholder="adobo" />
-        </FormGroup>
-        {' '}
-        <Button>Submit</Button>
+          <FormControl
+            type="text"
+            placeholder="adobo"
+            onChange={e => this.setState({ dish: e.target.value })}
+          />
+        </FormGroup>{" "}
+        <Button onClick={() => this.search()}>Submit</Button>
       </Form>
     );
   }
